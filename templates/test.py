c4a9527c57a40ea8f29e759f007e66abf9e2dc5e
Raahim <144481724+Faraaz304@users.noreply.github.com>
2025-05-14 17:15:36 +0530

    change

--- templates/test.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
